341caf8188bc9ea2943cbef2a07a07585a19e9b5
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 14:51:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function happyHolidays() {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
